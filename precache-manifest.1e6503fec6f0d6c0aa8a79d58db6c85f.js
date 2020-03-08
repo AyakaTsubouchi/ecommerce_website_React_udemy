4e7ec1c8582eeea1e1a7667b905c5c8737b05836
Ayaka Tsubouchi <ayakawell@gmail.com>
2020-03-08 15:12:31 -0700

    Updates

---
+++ precache-manifest.1e6503fec6f0d6c0aa8a79d58db6c85f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f37dd7796927df81713623768f871312",
+    "url": "/ecommerce_website_React_udemy/index.html"
+  },
+  {
+    "revision": "ee11a2684d32a17682db",
+    "url": "/ecommerce_website_React_udemy/static/css/main.a2f53dc6.chunk.css"
+  },
+  {
+    "revision": "1eba20a67589ac32db6e",
+    "url": "/ecommerce_website_React_udemy/static/js/2.83d39196.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ecommerce_website_React_udemy/static/js/2.83d39196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee11a2684d32a17682db",
+    "url": "/ecommerce_website_React_udemy/static/js/main.8a13017c.chunk.js"
+  },
+  {
+    "revision": "14c13ca16afc7dd7eb91",
+    "url": "/ecommerce_website_React_udemy/static/js/runtime-main.eba6f538.js"
+  }
+]);
